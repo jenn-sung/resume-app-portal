420850001ad9195c851e476f2e717b41bfed9e20
JennSung <jennifer.sung@colorado.edu>
2018-01-22 17:55:48 -0700

    adding of login in html and js

---
+++ public/index.js
@@ -0,0 +1,53 @@
+
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var router = new VueRouter({
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/signup", component: SignupPage },
+    { path: "/login", component: LoginPage }
+  ]
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
+});
